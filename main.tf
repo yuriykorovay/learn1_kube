118664e15954d1dea6bc7d3473a3ef4b6faf1149
yuriykorovay <yuriykorovay@gmail.com>
2024-07-25 20:27:26 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,3 +18,16 @@ variable "hcloud_token" {
 provider "hcloud" {
   token = var.hcloud_token
 }
+
+
+resource "hcloud_network" "private_network" {
+  name     = "kubernetes-cluster"
+  ip_range = "10.0.0.0/16"
+}
+
+resource "hcloud_network_subnet" "private_network_subnet" {
+  type         = "cloud"
+  network_id   = hcloud_network.private_network.id
+  network_zone = "eu-central"
+  ip_range     = "10.0.1.0/24"
+}
