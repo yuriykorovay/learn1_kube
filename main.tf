e3a14a3401809c4deaba089f53f0e622578ba534
yuriykorovay <yuriykorovay@gmail.com>
2024-07-25 20:30:26 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,3 +31,26 @@ resource "hcloud_network_subnet" "private_network_subnet" {
   network_zone = "eu-central"
   ip_range     = "10.0.1.0/24"
 }
+
+
+resource "hcloud_server" "master-node" {
+  name        = "master-node"
+  image       = "ubuntu-22.04"
+  server_type = "cx22"
+  location    = "fsn1"
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = true
+  }
+  network {
+    network_id = hcloud_network.private_network.id
+    # IP Used by the master node, needs to be static
+    # Here the worker nodes will use 10.0.1.1 to communicate with the master node
+    ip         = "10.0.1.1"
+  }
+  user_data = file("${path.module}/cloud-init.yaml")
+
+  # If we don't specify this, Terraform will create the resources in parallel
+  # We want this node to be created after the private network is created
+  depends_on = [hcloud_network_subnet.private_network_subnet]
+}
